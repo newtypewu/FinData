80e3eab51a29714c6e570cd7d8dfc14a4062afe3
Tien Yuan, Wu <tienyuan@findata.com.tw>
2016-05-19 15:32:29 +0800

    init project

---
+++ FinTest/FinTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinTest
+//
+//  Created by 吳天元 on 5/19/16.
+//  Copyright © 2016 吳天元. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
